ab0037a31b5914bab17f732c2f94534e6be52a8c
Jora Gevorgyan <joragevorgyan004@gmail.com>
2021-02-12 20:40:53 +0400

    counts loops in each f-n with their intagrated loops

--- LoopCntr/LoopCntr.cpp
+++ LoopCntr/LoopCntr.cpp
@@ -1,27 +1,61 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+
 #include "llvm/Analysis/LoopPass.h"
+#include "llvm/Analysis/LoopInfo.h"
 
 namespace {
-    using ULLONG = unsigned long long;
+    class LoopCntr : public llvm::FunctionPass {
+        public:
+            static char ID;
+            LoopCntr() : FunctionPass(ID) {}
 
-    struct LoopCntr : public llvm::LoopPass {
-        static char ID;
-        static ULLONG countOfObjects; 
+            void getAnalysisUsage(llvm::AnalysisUsage &AU) const override {
+                AU.setPreservesCFG();
+                AU.addRequired<llvm::LoopInfoWrapperPass>();
+            }
 
-        LoopCntr() : LoopPass(this->ID) {}
-        ~LoopCntr() {
-            llvm::errs() << "You have "
-                << this->countOfObjects << " loop(s) in your project!\n";
-        }
-        virtual bool runOnLoop(llvm::Loop* L, llvm::LPPassManager& LPM) override {
-            ++this->countOfObjects;
-            return false;
-        }
-    }; // end od LoopCntr struct
-    
-    ULLONG LoopCntr::countOfObjects = 0;
+            virtual bool runOnFunction(llvm::Function& F) override {
+                llvm::LoopInfoBase< llvm::BasicBlock, llvm::Loop > *curLoopBase
+                    = &getAnalysis< llvm::LoopInfoWrapperPass >().getLoopInfo();
+
+                const std::vector< llvm::Loop*, std::allocator< llvm::Loop* >>* topLoops 
+                    = &curLoopBase->getTopLevelLoops();
+
+                llvm::errs() << "Function '" << F.getName()
+                    << "' has " << topLoops->size() << " top level loop(s).\n";
+                size_t loopNum = 1;
+                for(auto it = topLoops->begin(); it != topLoops->end(); ++it, ++loopNum) {
+                    llvm::errs() << loopNum << ": ";
+                    this->countIntagratedLoops(*it);
+                }
+
+                llvm::errs() << "\n";
+
+                return false;
+            }
+
+        private:
+            void countIntagratedLoops(llvm::Loop* L, size_t loopLevel = 1) {
+                const std::vector< llvm::Loop*, std::allocator< llvm::Loop* >>* loops
+                    = &L->getSubLoops();
+
+                llvm::errs() << loops->size() << " intagrated loop(s).\n";
+                
+                size_t curLoopNum = 1;
+                std::string designStr(loopLevel * 4, '=');
+
+                for(auto it = loops->begin(), endIt = loops->end(); it != endIt; ++it) {
+                    llvm::errs() << designStr << " " << curLoopNum++ << ": ";
+                    countIntagratedLoops(*it, loopLevel + 1);
+                }
+            }
+
+    }; // end of LoopCntr struct
+
     char LoopCntr::ID = 0;
-    llvm::RegisterPass< LoopCntr > X("CountLoops", "Counts all loops", false, false);
+    llvm::RegisterPass< LoopCntr > X("CountLoops", "Counts all loops of each function",
+            false, false);
 } // end of anonymous namespace
 
